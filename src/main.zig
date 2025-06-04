4af01a3a90068b971db470a9fcd61386357ea02d
MetaSky <creator_meta_sky@163.com>
2025-06-04 09:13:46 -0400

    zig run ok.

--- src/main.zig
+++ src/main.zig
@@ -1,48 +1,43 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-
-    std.debug.print("hello world\n", .{});
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("rustcoro_lib");
+const STACK_SIZE: usize = 1024;
+
+const ThreadContext = struct {
+    rsp: u64,
+};
+
+fn hello() noreturn { // todo: - why can't use callconv(.C)
+    std.debug.print("hello wake up on a new stack\n", .{});
+    while (true) {}
+}
+
+extern fn gt_switch(new_ctx: *const ThreadContext) void;
+comptime {
+    asm (@embedFile("switch.S"));
+}
+
+pub fn main() !void {
+    var ctx = ThreadContext{
+        .rsp = 0,
+    };
+
+    std.debug.print("size = {d}\n", .{@sizeOf(u64)});
+
+    const stack = try std.heap.page_allocator.alignedAlloc(u8, 16, STACK_SIZE);
+    defer std.heap.page_allocator.free(stack);
+
+    // 1. 获取栈底位置
+    const stack_bottom = @intFromPtr(stack.ptr) + STACK_SIZE; // get stack bottom
+
+    // // 2. 确保16字节对齐
+    // const aligned_base = stack_bottom & ~@as(usize, 15);
+
+    const sb_aligned = (stack_bottom - @sizeOf(u64)) & ~@as(usize, 15); // todo: - 16 vs 8
+
+    @as(*u64, @ptrFromInt(sb_aligned)).* = @intFromPtr(&hello);
+
+    ctx.rsp = sb_aligned;
+    gt_switch(&ctx);
+
+    std.debug.print("hello zig coro\n", .{});
+}
