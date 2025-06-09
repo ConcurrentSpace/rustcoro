5e1701e88c143313ba0851f5bf753db001eb88a9
MetaSky <creator_meta_sky@163.com>
2025-06-09 18:56:32 -0400

    update before after

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ global_asm!(
     options(att_syntax) // 这里你可以修改为 intel | at&t 语法
 );
 
-const STACK_SIZE: isize = 48;
+const STACK_SIZE: isize = 1024;
 static mut S_PTR: *const u8 = 0 as *const u8;
 
 fn print_stack(filename: &str) {
     let mut file = std::fs::File::create(filename).unwrap();
     unsafe {
         for i in (0..STACK_SIZE).rev() {
+            // println!("index = {}", i);
             writeln!(file, "mem: {}, value: {}", 
                 S_PTR.offset(i as isize) as usize,
                 *S_PTR.offset(i as isize)
@@ -54,33 +55,35 @@ unsafe extern "C" {
 // `&mut T`可以隐式转换为`*const T`(不可变原始指针)
 // Rust允许可变引用到不可变指针的自动转换
 // 这种转换是安全的，因为不会通过不可变指针修改数据
-// fn main() {
-//     let mut ctx = ThreadContext::default();
-//     let mut stack = vec![0_u8; STACK_SIZE as usize];
-//     let stack_ptr = stack.as_mut_ptr();
-
-//     unsafe {
-//         S_PTR = stack_ptr;
-//         std::ptr::write(stack_ptr.offset(-16) as *mut u64, hello as u64);
-//         print_stack("before.txt");
-//         ctx.rsp = stack_ptr.offset(-16) as u64; 
-//         gt_switch(&mut ctx)
-//     };
-// }
-
-pub fn main() {
+fn main() {
     let mut ctx = ThreadContext::default();
     let mut stack = vec![0_u8; STACK_SIZE as usize];
     let stack_ptr = stack.as_mut_ptr();
+
+    // todo: - why not align
     unsafe {
-        let stack_bottom = stack.as_mut_ptr().offset(STACK_SIZE);
-        let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
-        S_PTR = sb_aligned;
+        S_PTR = stack_ptr;
         std::ptr::write(stack_ptr.offset(STACK_SIZE - 16) as *mut u64, hello as u64);
-        print_stack("BEFORE.txt");
-        ctx.rsp = stack_ptr.offset(STACK_SIZE - 16) as u64;
-        gt_switch(&mut ctx);
-    }
+        print_stack("before.txt");
+        ctx.rsp = stack_ptr.offset(STACK_SIZE - 16) as u64; 
+        println!("rsp = {}", ctx.rsp);
+        gt_switch(&mut ctx) // todo: - jump where
+    };
 }
+
+// pub fn main() {
+//     let mut ctx = ThreadContext::default();
+//     let mut stack = vec![0_u8; STACK_SIZE as usize];
+//     let stack_ptr = stack.as_mut_ptr();
+//     unsafe {
+//         let stack_bottom = stack.as_mut_ptr().offset(STACK_SIZE);
+//         let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
+//         S_PTR = sb_aligned;
+//         std::ptr::write(stack_ptr.offset(STACK_SIZE - 16) as *mut u64, hello as u64);
+//         print_stack("before.txt");
+//         ctx.rsp = stack_ptr.offset(STACK_SIZE - 16) as u64;
+//         gt_switch(&mut ctx);
+//     }
+// }
 
 // todo: codelldb debug problem
